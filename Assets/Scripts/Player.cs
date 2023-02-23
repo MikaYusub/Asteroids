5257e9596ca0b13bd4c4aaa375008b84acd4034c
Mikayil Yusubov <mikayil.yusubov@gmail.com>
2023-02-23 13:05:45 +0400

    Added player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rigidbody;
+    private bool _thrusting;
+    public float thrustSpeed = 1.0f;
+    private float _turndirection;
+    public float turnSpeed = 1.0f;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _thrusting = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turndirection = 1.0f;
+        }
+        else
+        {
+            _turndirection =
+                Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow) ? -1.0f : 0.0f;
+        }
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        if (_thrusting)
+        {
+            _rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+        if (_turndirection != 0)
+        {
+            _rigidbody.AddTorque(_turndirection * this.turnSpeed);
+        }
     }
 }
